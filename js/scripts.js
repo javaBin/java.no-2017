1f25f42816f1e60761126f566fc4aa850303e6ec
Håkon Botnmark Jahre <hakonj@mesan.no>
2015-09-06 00:08:07 +0200

    fikset dash

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,24 @@
  * Created by hakonj on 05/09/15.
  */
 
-$(function() {
+$(function () {
     var element = $("header");
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
 
 
-        if(scroll > 0) {
+        if (scroll > 0) {
             element.addClass("sticky");
         } else {
             element.removeClass("sticky");
         }
     });
 
-    $("header a").click(function() {
+    $("header a").click(function () {
         var anchor = this.href.split("#")[1];
         var position = 0;
 
-        if(anchor !== "") {
+        if (anchor !== "") {
             position = $("#" + anchor).offset().top;
         }
 
@@ -29,4 +29,53 @@ $(function() {
 
         return false;
     });
+
+    var url = "http://api.meetup.com/2/events?group_id=7480032%2C8449272%2C7371452%2C4060032%2C10847532%2C1764379&status=upcoming&order=time&limited_events=False&desc=false&offset=0&format=json&page=20&fields=&sig_id=14499833&sig=5cd7131eecfb2f8b4581762dd8c58c77c266d23d";
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "jsonp",
+        success: function (data) {
+            $(".meetup-loading").hide();
+            var eventsList = "";
+            if (data.results.length == 0) {
+                eventsList = '<section><h2>Vi har for tiden ingen planlagte meetups.</h2></section>';
+            } else {
+                $.each(data.results, function (i, item) {
+                    var remainingSeats = "";
+                    if (item.rsvp_limit) {
+                        remainingSeats = '<span>' + (item.rsvp_limit - item.yes_rsvp_count) + ' ledige plasser</span>';
+                    }
+                    var venueCity = typeof item.venue !== "undefined" ? item.venue.city : null;
+                    var venueName = typeof item.venue !== "undefined" ? item.venue.name : null;
+                    var venueString = venueCity === null || venueName === null ? "Foreløpig ingen lokasjon." : venueName + ', ' + venueCity;
+                    eventsList = eventsList +
+                        '<div class="meetup">' +
+                        '   <figure>' +
+                        '       <img class="map" src="http://maps.googleapis.com/maps/api/staticmap?center=' + venueCity + ',Norway&zoom=10&size=75x75&sensor=false"/>' +
+                        '       <figcaption>' + item.group.name + '</figcaption>' +
+                        '   </figure>' +
+                        '   <div>' +
+                        '       <p class="region">' + item.group.name + '</p>' +
+                        '       <p><a class="eventlink" href="' + item.event_url + '">' + item.name + '</a></p>' +
+                        '       <p class="venue">' + venueString + '</p>' +
+                        '       <p class="timeanddate">' + moment(new Date(item.time)).format("dddd, MMMM DD, HH:mm") + '</p>' +
+                        '   </div>' +
+                        '   <div>' +
+                        '       <span class="rsvp-count">' + item.yes_rsvp_count + '</span>' +
+                        '       <span>påmeldte</span>' +
+                        remainingSeats +
+                        '   </div>' +
+                        '</div>';
+                });
+            }
+
+            $("#meetup").html($("#meetup").html() + '<div class="flex-box">' + eventsList + '</div>');
+        },
+        beforeSend: setHeaders
+    });
+
+    function setHeaders(xhr) {
+        xhr.setRequestHeader("Origin", "http://java.no");
+    }
 });
